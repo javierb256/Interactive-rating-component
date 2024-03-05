a89daaa635aa67dc1c0f46ebfe5b98a040b479bc
= <44247277+javierb256@users.noreply.github.com>
2024-03-04 21:53:22 -0800

    finished adding interactivity when selecting and submitting form

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 const start = document.querySelector(".start");
 const submit = document.querySelector(".submit");
-const resultContainer = document.querySelector('.result-container');
+const resultContainer = document.querySelector(".result-container");
+const buttons = document.querySelectorAll(".option");
+const finalValue = document.querySelector("#finalValue");
+let value = 0;
 
-// resultContainer.style.display = 'block'
+// add an event listener to each rating button
+buttons.forEach(button => {
+  button.addEventListener("click", e => {
+    value = e.target.value;
+  });
+});
 
-submit.addEventListener("click", ()=> {
-    start.style.display = 'none';
-    resultContainer.style.display = 'block';
-})
+// When survey is submitted make initial screen disappear and replace with results page
+submit.addEventListener("click", () => {
+  if (value == 0) {
+    alert("Please select a rating");
+  } else {
+    start.style.display = "none";
+    resultContainer.style.display = "block";
+    finalValue.textContent = value;
+  }
+});
